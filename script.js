b09037ee413721e201a27a448beb1ccd15465a18
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-21 14:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.age = age;
+		this.name = name;
+	}
+	get getName(){
+		return this.name;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name+" "+"is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(this.name+" "+"is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
