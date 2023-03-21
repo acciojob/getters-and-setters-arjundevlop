6d543c31b84c3e84e20bb0ea03d0173c4131b880
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-21 15:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	get name(){
-		return this.name;
-	}
-	set age(age){
-		this._age = age;
-	}
-	get age() {
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
+
+  get age() {
     return this._age;
   }
-	
 }
 
 class Student extends Person {
-	study() {
+  study() {
     console.log(`${this._name} is studying`);
   }
 }
 
 class Teacher extends Person {
-	teach() {
+  teach() {
     console.log(`${this._name} is teaching`);
   }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
